1e7c3b5a9752ff385e439fe0fb5870b033cb811d
SkandaAithal-1 <133839044+SkandaAithal-1@users.noreply.github.com>
2023-05-25 21:04:27 +0530

    Update main.py

--- ppt-control/Script/main.py
+++ ppt-control/Script/main.py
@@ -1,10 +1,23 @@
 from function_module import *
+import mss
+import cv2
+import numpy as np
+
+sct=mss.mss()
 
 def main()->None:
     '''
     The main function
     '''
-    # Google meet stream input
+    monitor={"top":0, "left":960, "width":953, "height":1700} #The part of the screen to be captured
+    while True:
+        img=np.array(sct.grab(monitor)) #img contains the screenshot
+        #To display the screen that is being recorded
+        '''cv2.imshow("Screenshot", img)
+        k=cv2.waitKey(1)
+        if (k==ord("q")):
+            break
+    cv2.destroyAllWindows'''
     # analyze frames and execute actions
     return None
 
