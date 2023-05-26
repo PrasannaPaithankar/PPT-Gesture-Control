050aab3045e24351205d0bfc7cba348a5a23f0b9
SkandaAithal-1 <133839044+SkandaAithal-1@users.noreply.github.com>
2023-05-26 20:33:33 +0530

    Update main.py
    Used tkinter to take the URL input and used it to extract data from phone's camera. The URL is provided by IP webcam.

--- ppt-control/Script/main.py
+++ ppt-control/Script/main.py
@@ -1,23 +1,48 @@
 from function_module import *
-import mss
+import tkinter as tk
 import cv2
-import numpy as np
 
-sct=mss.mss()
+class GUI:
+    def __init__(self):
+        self.root=tk.Tk()
 
+        self.frame=tk.Frame(self.root)
+        self.frame.pack()
+
+        self.label=tk.Label(self.frame, text="Enter the URL")
+        self.label.pack()
+
+        self.txtbox=tk.Text(self.root,height=1)
+        self.txtbox.pack(padx=10,pady=10)
+
+        self.button=tk.Button(self.root, text="Enter", width=10, command=self.Enter)
+        self.button.pack(pady=10)
+
+        self.root.mainloop()
+   def Enter(self):
+    '''
+        This will store the text entered in the textbox of tkinter interface and close the interface when the enter button is pressed
+    '''
+        self.string=self.txtbox.get("1.0", tk.END)  
+        if (len(self.string)!=1):
+            self.root.destroy()
+     
 def main()->None:
     '''
     The main function
     '''
-    monitor={"top":0, "left":960, "width":953, "height":1700} #The part of the screen to be captured
+    tkinter_object=GUI()
+    vid=cv2.VideoCapture(tkinter_object.string+"/video")
+    
     while True:
-        img=np.array(sct.grab(monitor)) #img contains the screenshot
-        #To display the screen that is being recorded
-        '''cv2.imshow("Screenshot", img)
+        ret,frame=vid.read()
+        if not ret:
+            continue
+        cv2.imshow("Video", video)
         k=cv2.waitKey(1)
         if (k==ord("q")):
             break
-    cv2.destroyAllWindows'''
+    
     # analyze frames and execute actions
     return None
 
